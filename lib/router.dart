e6ec177ddf1dde67461e3c36434fc6c636f6d1b2
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2025-05-15 15:15:00 +0900

    router 개념 이해 및 적용 단계

--- lib/router.dart
+++ lib/router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:studyf/main.dart';
 
 final router = GoRouter(
+//  initialLocation: "/splash", 이것 처럼 첫 path가 굳이 /아니여도 된다
     routes: [
       GoRoute(path: "/", builder: (context, state) { return MyHomePage(title: "하이"); } ),
     ]
