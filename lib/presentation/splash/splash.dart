efe87e71d3362485a55346c1f99b19a05ca1fb93
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2025-05-16 13:49:52 +0900

    Image 추가 방법, Text 활용, SizedBox body - child - children의 이해

--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -14,6 +14,28 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: splashBackground,
+      body: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/ic/splash.png',
+              width: 46.0,
+              height: 46.0,
+            ),
+
+            SizedBox(height: 30),
+
+            Text(
+              "쓰따디 스플래쉬",
+              style: TextStyle(
+                color: Colors.black,
+                fontStyle: FontStyle.italic
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
