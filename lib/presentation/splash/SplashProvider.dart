571de62d38d43527d7d3306df57ab6cb507f9c0f
rudtjr1106 <83599356+rudtjr1106@users.noreply.github.com>
2025-05-22 15:25:03 +0900

    firebase 연결

--- lib/presentation/splash/SplashProvider.dart
+++ lib/presentation/splash/SplashProvider.dart
@@ -2,10 +2,5 @@ import 'package:flutter/cupertino.dart';
 import 'package:studyf/base/BaseProvider.dart';
 
 class SplashProvider with ChangeNotifier implements BaseProvider {
-  var counter = 0;
 
-  void increment(){
-    counter++;
-    notifyListeners();
-  }
 }
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -16,14 +16,10 @@ class _SplashScreenState extends BaseScreenState<SplashScreen, SplashProvider> {
   @override
   void initView(SplashProvider provider) {
     super.initView(provider);
-    Future.delayed(const Duration(seconds: 5), () {
-      provider.increment();
-    });
-
   }
+
   @override
   Widget buildBody(BuildContext context, SplashProvider provider) {
-    print(provider.counter);
     return Scaffold(
       backgroundColor: splashBackground,
       body: SizedBox.expand(
